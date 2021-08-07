8353ff42dcb052c238594ef884c5c405b0baa8c1
Arjun Aravind <arjun.aravind1998@gmail.com>
2021-08-08 00:09:30 +0530

    refactor main.go file

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	//"os"
+	"os"
 	"io"
 	"fmt"
-	//"json"
+	"strconv"
 	"github.com/ArjArav98/Issue/src/api"
 )
 
 func main () {
-	//argsWithoutProg = os.Args[1:]
-	fmt.Printf(showIssue(1421))
+	argsWithoutProg := os.Args[1:]
+
+	switch argsWithoutProg[0] {
+		case "show":
+			switch argsWithoutProg[1] {
+				default:
+					issueId, err := strconv.ParseInt(argsWithoutProg[1], 10, 32)
+					if err != nil {
+						printError(err)
+					}
+
+					response, err := showIssue(int(issueId))
+					if err != nil {
+						printError(err)
+					}
+
+					fmt.Println(response)
+			}
+		default: fmt.Println("Command not recognised.")
+	}
+
 }
 
-func showIssue (issueId int) string {
+func printError (err error) {
+	fmt.Printf("Fatal error: %v.", err)
+}
+
+func showIssue (issueId int) (string, error) {
 	repositoryId := "8540679"
 
 	url, err := api.GenerateRequestUrl("get-single-issue", repositoryId, issueId)
-
 	if err != nil {
-		return fmt.Sprintf("%v", err)
+		return "", err
 	}
 
 	response, err := api.PerformGetRequest(url)
-
 	defer response.Body.Close()
-	body, err := io.ReadAll(response.Body)
 
-	return string(body)
+	body, err := io.ReadAll(response.Body)
+	return string(body), nil
 }
