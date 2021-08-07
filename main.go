3b4b916ae3c2cc9c1d6ca8a5eb55c1219805b65c
Arjun Aravind <arjun.aravind1998@gmail.com>
2021-08-07 22:05:19 +0530

    update function to get request

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-	"os"
+	//"os"
+	"io"
 	"fmt"
 	//"json"
-	//"http"
+	"net/http"
 )
 
-func main() {
-	fmt.Printf("%v", os.Args)
+func main () {
+	//argsWithoutProg = os.Args[1:]
+	fmt.Printf(showIssue(1421))
+}
+
+func showIssue (issueId int) string {
+	token := "zBX_EB6cpPgv3sJ7Fm6g"
+	repositoryId := "8540679"
+
+	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%v/issues/%v", repositoryId, issueId)
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return "oops"
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "oops2"
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	return string(body)
 }
