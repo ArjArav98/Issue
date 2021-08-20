3aa593f60f551a37f5ce5efbfe9fe3e55fa25512
Arjun Aravind <arjun.aravind1998@gmail.com>
2021-08-20 18:37:33 +0530

    add expansion function for shortcuts

--- src/parse/parse.go
+++ src/parse/parse.go
@@ -4,13 +4,24 @@ import (
 	"errors"
 	"strings"
 	"net/url"
+	"github.com/ArjArav98/Issue/src/defaults"
 )
+
+/*-------------------*/
+/* PRIVATE VARIABLES */
+/*-------------------*/
+
+var SearchShortcutExpansionFunctions map[string]func()[]string = map[string]func()[]string{
+	"--my-issues": defaults.ArgsForCurrentUserIssues,
+	"--my-open-issues": defaults.ArgsForCurrentUserOpenIssues,
+}
 
 /*-------------------*/
 /* EXPOSED FUNCTIONS */
 /*-------------------*/
 
 func CliArgumentsToQueryParams (arguments []string) (url.Values, error){
+	arguments = expandSearchShortcuts(arguments)
 	queryParams := url.Values{}
 
 	if len(arguments)%2 != 0 {
@@ -31,4 +42,37 @@ func CliArgumentsToQueryParams (arguments []string) (url.Values, error){
 	}
 
 	return queryParams, nil
+}
+
+/*-------------------*/
+/* PRIVATE FUNCTIONS */
+/*-------------------*/
+
+func expandSearchShortcuts (arguments []string) []string {
+	var newArguments []string
+
+	for _, argument := range arguments {
+		if argumentIsShortcut(argument) {
+			expansionFunction, _ := SearchShortcutExpansionFunctions[argument]
+			newArguments = append(newArguments, expansionFunction()...)
+			continue
+		}
+
+		newArguments = append(newArguments, argument)
+	}
+
+	return newArguments
+}
+
+func argumentIsShortcut (argument string) bool {
+	_, present := SearchShortcutExpansionFunctions[argument]
+	return present
+}
+
+func argumentIsSearchKey (argument string) bool {
+	return strings.HasPrefix(argument, "--")
+}
+
+func argumentIsSearchValue (argument string) bool {
+	return !strings.HasPrefix(argument, "--")
 }
